99eddd9d9fb235bd054761a15bc01a904f0ce475
RelativeBinary <patrick.bornayx@protonmail.com>
2018-10-24 21:54:28 +1100

    Initial Commit

---
+++ src/mocktextgenerator/MockTextGenerator.java
@@ -0,0 +1,16 @@
+package mocktextgenerator;
+
+/**
+ *
+ * @author Coffee-2
+ */
+public class MockTextGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
